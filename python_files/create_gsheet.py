e36ae63b99cdb78e17e67144099d33117840cfae
Foxdapple <110433393+Foxdapple@users.noreply.github.com>
2024-08-23 12:43:55 +1200

    Created a function to get base key
    added function into create_gsheet.py to get the character name from the json file.

--- python_files/create_gsheet.py
+++ python_files/create_gsheet.py
@@ -5,3 +5,25 @@ ToDo:
     Make it easy to add information to the gsheet
 '''
 
+a_json = {
+    "Mraess Equirrion": {
+        "Level": "8",
+        "Race": "Gold Dragonborn",
+        "classes/levels": [],
+        "HP": "60",
+        "AC": "17",
+        "Init": "+4",
+        "Speed": "30 ft",
+        "Languages": [],
+        "Stats": {},
+        "Skills": {},
+        "Casting": {},
+        "Description": {}
+    }
+}
+
+
+def get_character_name(json):
+    return list(json.keys())[0]
+
+print(get_character_name(a_json))
